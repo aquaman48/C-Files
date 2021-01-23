adbe7b8a0612f86c4db9885c16d44fd1c46f251b
Nicholas Waterman <63140071+aquaman48@users.noreply.github.com>
2021-01-23 12:24:07 -0800

    String Concatenation
    A simple base program that shows how concatenation works.

---
+++ Arrays-Strings/SimpleCat.c
@@ -0,0 +1,13 @@
+//Creating a base file that Concatenates strings 
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char cStr1[40] = "Concatenating Strings ";
+	char cStr2[] = "is oh so much fun! Am I right? "
+
+	printf("\n%s\n", strcat(cStr1, cStr2))
+	return 0;
+}
